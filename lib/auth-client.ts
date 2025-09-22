a3bb84c45204ec0c8b41489433ac88b36942a846
Ridhwan Hussain <ridhwanplayzzz@gmail.com>
2025-09-22 19:56:08 +0100

    Remove client plugin

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,8 +1,7 @@
 import { convexClient } from "@convex-dev/better-auth/client/plugins";
 import { adminClient, oidcClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
-import { overrideClient } from "./plugins/override/client";
 
 export const authClient = createAuthClient({
-    plugins: [convexClient(), adminClient(), oidcClient(), overrideClient()],
+    plugins: [convexClient(), adminClient()],
 });
