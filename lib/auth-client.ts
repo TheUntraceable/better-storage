960f32db0801a44396c6b09c51a5ebaa858b7828
Ridhwan Hussain <ridhwanplayzzz@gmail.com>
2025-09-22 19:56:22 +0100

    Remove unused client plugin

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { convexClient } from "@convex-dev/better-auth/client/plugins";
-import { adminClient, oidcClient } from "better-auth/client/plugins";
+import { adminClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
