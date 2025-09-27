57230052d3c208dda2d6aa9eaa0313ff6f2c0c72
Ridhwan Hussain <ridhwanplayzzz@gmail.com>
2025-09-27 08:11:32 +0100

    return attached user if not found from context

--- convex/autumn.ts
+++ convex/autumn.ts
@@ -3,19 +3,21 @@ import { components } from "./_generated/api";
 
 export const autumn = new Autumn(components.autumn, {
     secretKey: process.env.AUTUMN_SECRET_KEY ?? "",
-    
-	identify: async (ctx: any) => {
-        const user = await ctx.auth.getUserIdentity() || ctx.user
-        console.log(user, ctx)
-		if (!user) return null
 
-		return {
-			customerId: user.subject as string,
-			customerData: {
-				email: user.email as string,
-			},
-		};
-	},
+    identify: async (ctx: any) => {
+        const user = (await ctx.auth.getUserIdentity()) || ctx.user;
+
+		if (!user) {
+            return null;
+        }
+
+        return {
+            customerId: user.subject as string,
+            customerData: {
+                email: user.email as string,
+            },
+        };
+    },
 });
 
 export const { track, check, ...rest } = autumn.api();
